df8974d50ef3ffeb193cbab4f5f9374ac519d0f5
YogaStore <103028161+YogaStore@users.noreply.github.com>
2022-04-10 05:58:28 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,14 +5,14 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="RafliKunn"
-global.ownernumber = "62895410068780"
-global.botname = "RflBotz Ofc"
+global.ownername ="Lenzz Store"
+global.ownernumber = "6285236138574"
+global.botname = "Lenzz Botz"
 global.thumbnail = fs.readFileSync("./image/elaina.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'rafliofcheh' //ganti pakai apikey kalian
 global.limit = {
-		free:20,
+		free:50,
 		premium:1000
 	}
 global.session_name = "session.json"
